c116f6db28a045f52d5e950874fcab7869131ece
jian899 <mjian528@gmail.com>
2023-05-20 22:02:31 +0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tail are you?: "))
+
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+print("You are"+str(height)+"cm tail")
