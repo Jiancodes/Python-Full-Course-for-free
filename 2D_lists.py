6530dee0e2bdd79411ba18df767b72d6a9fa00c7
jian899 <mjian528@gmail.com>
2023-06-29 08:43:00 +0800

    add files via upload

---
+++ 2D_lists.py
@@ -0,0 +1,8 @@
+# 2D lists = a list of lists
+
+drink = ["coffee", "soda", "tae"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drink, dinner, dessert]
+print(food[2][2])
