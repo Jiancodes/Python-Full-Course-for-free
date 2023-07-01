1edb972de8932d995054668543fe9405609c6d6c
jian899 <mjian528@gmail.com>
2023-07-01 10:26:57 +0800

    Add files via upload

---
+++ class_variables.py
@@ -0,0 +1,9 @@
+class Car:
+
+    wheels = 4  #class variable
+
+    def __init__(self,make,model,year,color):
+        self.make = make    #instance varible
+        self.model = model  #instance varible
+        self.year = year    #instance varible
+        self.color = color  #instance varible
